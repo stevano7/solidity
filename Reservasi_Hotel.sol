5c842cd1efa6048a06c3bca9349b384607991b54
stevano7 <104145137+stevano7@users.noreply.github.com>
2022-05-21 21:32:31 +0700

    Update Reservasi_Hotel.sol

--- Reservasi_Hotel.sol
+++ Reservasi_Hotel.sol
@@ -37,11 +37,33 @@ contract DataVaksin {
                 VacsMap[vaks.noKTP] = _statVac;
         }
 
+        function cekVaksinee(string memory _noKTP) public view returns (bool) {
+
+                bool exist;
+                
+                for (uint i = 0; i < vaksinee.length; i++) {
+
+                    if (keccak256(abi.encodePacked(_noKTP)) == keccak256(abi.encodePacked(vaksinee[i].noKTP))) {
+                        exist = true;
+                        break;
+
+                    } else {
+
+                        exist = false;
+                    }
+
+                }
+
+                 return exist;
+
+        }
+
         function getStatusVaksin(string memory _noKTP) public view returns (uint) {
 
                 
                 uint statNum;
 
+               
                 if (StatusVaksin.BELUM_VAKSIN == VacsMap[_noKTP]) statNum = 0;
                 if (StatusVaksin.VAKSIN_PERTAMA == VacsMap[_noKTP]) statNum = 1;   
                 if (StatusVaksin.VAKSIN_KEDUA == VacsMap[_noKTP]) statNum = 2;
@@ -49,6 +71,8 @@ contract DataVaksin {
                 return statNum;        
 
         }
+
+        
 
 }
 
@@ -59,8 +83,8 @@ contract Reservasi {
          string public noKTP;
          string public nama;
          uint public statusVaksin;
-         bool private isiData;
-         address addr = 0xD4Fc541236927E2EAf8F27606bD7309C1Fc2cbee;
+         bool public isiData;
+         address addr = 0x5Fcd5d9f6444dD23Ca2af792B58B041A14fB34EB;
          DataVaksin _datavac = DataVaksin(addr);
 
          event reserve(string noKTP, uint harga);
@@ -74,9 +98,32 @@ contract Reservasi {
 
         function setDataPemesan(string memory _noKTP, string memory _nama) public {
 
+            if ((keccak256(abi.encodePacked(_noKTP)) != keccak256(abi.encodePacked(noKTP))) && isiData) {
+
+                        revert("Harap Selesaikan Pemesanan Sebelumnya");
+                }
+
+
+                if (bytes(_noKTP).length == 0) {
+
+                        revert("Harap isi No KTP");
+                    }
+
+                if (bytes(_nama).length == 0) {
+
+                        revert("Harap isi Nama");
+                    }       
+
+                if (_datavac.cekVaksinee(_noKTP) == false) {
+
+                        revert("Data Vaksin Tidak Ditemukan");
+                }
+
+
+
                     noKTP = _noKTP;
                     nama = _nama;
-                    statusVaksin = _datavac.getStatusVaksin(_noKTP);
+                    statusVaksin = _datavac.getStatusVaksin(_noKTP);             
                     isiData = true;
          }
 
@@ -115,11 +162,18 @@ contract Reservasi {
 
 
 
-         function pesanKamar(address payable _hotel) payable public cekKamar cekisiData cekStatVak cekEther {
+         function pesanKamar(address payable _hotel) payable public cekKamar cekisiData cekEther cekStatVak {
+
+                //if (statusVaksin == 0) {
+                    
+                    //revert("Anda Belum Vaksin");
+                //}
+
                 jmlKamar--;
                 _hotel.transfer(msg.value);
                 emit reserve(noKTP, msg.value);
                 isiData = false;
+                
          }
 
          function cekJmlKamar() public view returns (uint) {
