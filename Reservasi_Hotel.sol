9385dda2dda32f986e775b2cb8bac4a218085ab3
stevano7 <104145137+stevano7@users.noreply.github.com>
2022-05-21 21:49:34 +0700

    Update Reservasi_Hotel.sol

--- Reservasi_Hotel.sol
+++ Reservasi_Hotel.sol
@@ -1,80 +1,7 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.7.0;
 
-contract DataVaksin {
-
-        enum StatusVaksin {
-            BELUM_VAKSIN,
-            VAKSIN_PERTAMA,
-            VAKSIN_KEDUA
-        }
-
-        struct Vaksinee {
-
-             string noKTP;
-             string nama;
-             StatusVaksin statVac;
-            
-        }
-
-        Vaksinee[] private vaksinee;
-
-        mapping (string => StatusVaksin) private VacsMap;
-
-      
-
-        function createVaksenee(string memory _noKTP, string memory _nama) public {
-
-            vaksinee.push(Vaksinee(_noKTP, _nama, StatusVaksin.BELUM_VAKSIN));
-            VacsMap[_noKTP] = StatusVaksin.BELUM_VAKSIN;
-
-        }
-
-        function updateStatusVaksin(uint _index, StatusVaksin _statVac) public {
-
-                Vaksinee storage vaks = vaksinee[_index];
-                vaks.statVac = _statVac;
-                VacsMap[vaks.noKTP] = _statVac;
-        }
-
-        function cekVaksinee(string memory _noKTP) public view returns (bool) {
-
-                bool exist;
-                
-                for (uint i = 0; i < vaksinee.length; i++) {
-
-                    if (keccak256(abi.encodePacked(_noKTP)) == keccak256(abi.encodePacked(vaksinee[i].noKTP))) {
-                        exist = true;
-                        break;
-
-                    } else {
-
-                        exist = false;
-                    }
-
-                }
-
-                 return exist;
-
-        }
-
-        function getStatusVaksin(string memory _noKTP) public view returns (uint) {
-
-                
-                uint statNum;
-
-               
-                if (StatusVaksin.BELUM_VAKSIN == VacsMap[_noKTP]) statNum = 0;
-                if (StatusVaksin.VAKSIN_PERTAMA == VacsMap[_noKTP]) statNum = 1;   
-                if (StatusVaksin.VAKSIN_KEDUA == VacsMap[_noKTP]) statNum = 2;
-
-                return statNum;        
-
-        }
-
-        
-
-}
+import "./Data_Vaksin.sol";
 
 contract Reservasi {
 
@@ -84,7 +11,7 @@ contract Reservasi {
          string public nama;
          uint public statusVaksin;
          bool public isiData;
-         address addr = 0x5Fcd5d9f6444dD23Ca2af792B58B041A14fB34EB;
+         address addr = 0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B;
          DataVaksin _datavac = DataVaksin(addr);
 
          event reserve(string noKTP, uint harga);
