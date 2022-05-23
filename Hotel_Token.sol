e087adeb4c463979a723f5fbac19429b4811c5f1
stevano7 <104145137+stevano7@users.noreply.github.com>
2022-05-23 14:52:02 +0700

    Create Hotel_Token.sol

---
+++ Hotel_Token.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.0;
+
+import "./ERC20.sol";
+
+contract HotelToken is ERC20 {
+
+    address public hotel;
+    constructor() ERC20('Hotel_Token', 'HTK') {
+
+        _mint(msg.sender, 10);
+        hotel = msg.sender;
+    }
+
+    function mint(address to, uint amount) external {
+        _mint(to, amount);
+    }
+
+    function burn(uint amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function trfToken(address to, uint amount) external {
+        transfer(to, amount);
+    }
+
+}
