68cc3d7a2c1ff12ec108e8446a456f26a6f9d455
stevano7 <104145137+stevano7@users.noreply.github.com>
2022-05-23 14:50:13 +0700

    Update Data_Vaksin.sol

--- Data_Vaksin.sol
+++ Data_Vaksin.sol
@@ -1,49 +1,53 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.7.0;
 
-contract DataVaksin {
+contract VaccineData {
 
-        enum StatusVaksin {
-            BELUM_VAKSIN,
-            VAKSIN_PERTAMA,
-            VAKSIN_KEDUA
+        enum vaccineStatus {
+            NOT_VACCINATED,
+            VACCINATED
         }
 
-        struct Vaksinee {
+        struct Vaccinee {
 
-             string noKTP;
-             string nama;
-             StatusVaksin statVac;
+             string vaccineeId;
+             string vaccineeName;
+             vaccineStatus statVac;
             
         }
 
-        Vaksinee[] private vaksinee;
+        event createvacc(string Vaccinee_Id, string Vaccinee_Name, vaccineStatus Vaccinee_Status);
+        event updatevacc(string Vaccinee_Id, string Vaccinee_Name, vaccineStatus Vaccinee_Status);
 
-        mapping (string => StatusVaksin) private VacsMap;
+        Vaccinee[] private vaccinee;
+
+        mapping (string => vaccineStatus) private VacsMap;
 
       
 
-        function createVaksenee(string memory _noKTP, string memory _nama) public {
+        function createVaccinee(string memory _vaccineeId, string memory _vaccineeName) public {
 
-            vaksinee.push(Vaksinee(_noKTP, _nama, StatusVaksin.BELUM_VAKSIN));
-            VacsMap[_noKTP] = StatusVaksin.BELUM_VAKSIN;
+            vaccinee.push(Vaccinee(_vaccineeId, _vaccineeName, vaccineStatus.NOT_VACCINATED));
+            VacsMap[_vaccineeId] = vaccineStatus.NOT_VACCINATED;
+            emit createvacc(_vaccineeId, _vaccineeName, vaccineStatus.NOT_VACCINATED);
 
         }
 
-        function updateStatusVaksin(uint _index, StatusVaksin _statVac) public {
+        function updateVaccineStatus(uint _index, vaccineStatus _statVac) public {
 
-                Vaksinee storage vaks = vaksinee[_index];
-                vaks.statVac = _statVac;
-                VacsMap[vaks.noKTP] = _statVac;
+                Vaccinee storage vacc = vaccinee[_index];
+                vacc.statVac = _statVac;
+                VacsMap[vacc.vaccineeId] = _statVac;
+                emit updatevacc(vacc.vaccineeId, vacc.vaccineeName, vacc.statVac);
         }
 
-        function cekVaksinee(string memory _noKTP) public view returns (bool) {
+        function checkVaccinee(string memory _vaccineeId) public view returns (bool) {
 
                 bool exist;
                 
-                for (uint i = 0; i < vaksinee.length; i++) {
+                for (uint i = 0; i < vaccinee.length; i++) {
 
-                    if (keccak256(abi.encodePacked(_noKTP)) == keccak256(abi.encodePacked(vaksinee[i].noKTP))) {
+                    if (keccak256(abi.encodePacked(_vaccineeId)) == keccak256(abi.encodePacked(vaccinee[i].vaccineeId))) {
                         exist = true;
                         break;
 
@@ -58,16 +62,15 @@ contract DataVaksin {
 
         }
 
-        function getStatusVaksin(string memory _noKTP) public view returns (uint) {
+        function getVaccineStat(string memory _vaccineeId) public view returns (uint) {
 
                 
                 uint statNum;
 
                
-                if (StatusVaksin.BELUM_VAKSIN == VacsMap[_noKTP]) statNum = 0;
-                if (StatusVaksin.VAKSIN_PERTAMA == VacsMap[_noKTP]) statNum = 1;   
-                if (StatusVaksin.VAKSIN_KEDUA == VacsMap[_noKTP]) statNum = 2;
-
+                if (vaccineStatus.NOT_VACCINATED == VacsMap[_vaccineeId]) statNum = 0;
+                if (vaccineStatus.VACCINATED == VacsMap[_vaccineeId]) statNum = 1;   
+                
                 return statNum;        
 
         }
