c4039437dde9464541c9b1c7ead2a85b7b5820e6
stevano7 <104145137+stevano7@users.noreply.github.com>
2022-05-28 21:55:51 +0700

    Update HTK_Exchange.sol

--- HTK_Exchange.sol
+++ HTK_Exchange.sol
@@ -5,10 +5,13 @@ import "./Hotel_Token.sol";
 
 contract HTKExchange {
 
-    address tknAddr = 0xd9145CCE52D386f254917e481eB44e9943F39138;
-    address HTKAddr = 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB;
+    address tknAddr = 0xd7B63981A38ACEB507354DF5b51945bacbe28414;
+    address payable private HTKAddr = 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB;
     HotelToken _hotelTkn = HotelToken(tknAddr);
     uint private HTKPrice;
+    uint private totalPay;
+
+    event buytoken(address buyer, address seller, uint HTK_Amount, uint Total_Price);
 
 
     constructor(uint _HTKPrice) {
@@ -23,9 +26,14 @@ contract HTKExchange {
     }
 
 
-    function buyHTK() payable public {
+    function buyHTK(uint HTK) payable public {
 
-         require(msg.value >= HTKPrice, "Not Enough Balance");
+        totalPay = HTK * HTKPrice;
+
+        require(msg.value >= totalPay, "Not Enough Balance");
+        HTKAddr.transfer(msg.value);
+        _hotelTkn.trfToken(HTKAddr, msg.sender, HTK);
+        emit buytoken(msg.sender, HTKAddr, HTK, totalPay);
 
     }
 
