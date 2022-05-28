88ca22bbebe8e0c6c3256338899d55743e194dc9
stevano7 <104145137+stevano7@users.noreply.github.com>
2022-05-28 23:36:02 +0700

    Update Guest_Data.sol

--- Guest_Data.sol
+++ Guest_Data.sol
@@ -13,10 +13,9 @@ contract GuestData {
                 string guestName;
                 string bookCode;
                 address addr;
-                uint tknBalance;
             }
 
-        event guestdata(uint Booking_Time, string Guest_Id, string Guest_Name, string Booking_Code, address Guest_Addr, uint256 Guest_Token);
+        event guestdata(uint Booking_Time, string Guest_Id, string Guest_Name, string Booking_Code, address Guest_Addr);
 
         constructor() {
              guestCount = 0;
@@ -24,11 +23,11 @@ contract GuestData {
 
          Guest[] public guest;
 
-        function inputGuest(uint _bookTime, string memory _guestId, string memory _guestName, string memory _bookCode, address _addr, uint256 _tknBalance) external {
+        function inputGuest(uint _bookTime, string memory _guestId, string memory _guestName, string memory _bookCode, address _addr) external {
 
              guestCount++;
-             guest.push(Guest(_bookTime, guestCount, _guestId, _guestName, _bookCode, _addr, _tknBalance));
-             emit guestdata (_bookTime, _guestId, _guestName, _bookCode, _addr, _tknBalance);
+             guest.push(Guest(_bookTime, guestCount, _guestId, _guestName, _bookCode, _addr));
+             emit guestdata (_bookTime, _guestId, _guestName, _bookCode, _addr);
 
          }
 
@@ -52,13 +51,5 @@ contract GuestData {
 
              return guest[guest.length - 1].bookTime;
          }
-
-
-
-        function getGuestToken() public view returns (uint256) {
-
-            return guest[guest.length - 1].tknBalance;
-        }
-
          
 }
